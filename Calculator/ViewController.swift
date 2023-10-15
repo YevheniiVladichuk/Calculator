2532681cd5b3bf2cf20f56f70a2f9651534d6062
Yevhenii Vladichuk <gorillakong@icloud.com>
2023-10-15 18:59:01 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Yevhenii Vladichuk on 15/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
